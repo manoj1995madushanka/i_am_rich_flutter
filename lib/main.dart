2f602faebf25188f299320655122320f977db5f5
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-13 13:57:58 +0530

    add network image to body

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    // for scaffold documentation check : https://api.flutter.dev/flutter/material/Scaffold-class.html
     home: Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: const Text("I am Rich"),
         backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              "https://upload.wikimedia.org/wikipedia/commons/6/6e/Gal_Gadot_2016_lighting_corrected.jpg"),
+        ),
       ),
     ),
   ));
