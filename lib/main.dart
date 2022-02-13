4defeb741a338321d1d04c1685e51699b23be351
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-13 13:34:55 +0530

    change scaffold background color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: const Text("I am Rich"),
         backgroundColor: Colors.blueGrey[900],
